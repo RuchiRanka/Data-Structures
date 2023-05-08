5b5ee59c39a1ec11f040d1b37d90f09c0e47df48
RuchiRanka <108867887+RuchiRanka@users.noreply.github.com>
2023-05-08 15:52:22 +0530

    Create matrix-diagonal-sum.java

---
+++ matrix-diagonal-sum.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int diagonalSum(int[][] mat) {
+        int sum = 0;
+        int i=0, j=mat.length-1;
+        while(i<mat.length && j>=0) {
+            sum+=mat[i][j];
+            i++;
+            j--;
+        }
+
+        for(int k=0; k<mat.length; k++) {
+            sum+=mat[k][k];
+        }
+
+        if(mat.length%2!=0) {
+            int index = mat.length/2;
+            sum -= mat[index][index];
+        }
+        return sum;
+    }
+}
