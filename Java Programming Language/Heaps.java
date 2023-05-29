e5f804eca272dbe271d0d8f3792a8278a7b81b4c
RuchiRanka <ruchiranka2103@gmail.com>
2023-05-29 22:12:01 +0530

    commit6

--- Java Programming Language/Heaps.java
+++ Java Programming Language/Heaps.java
@@ -69,18 +69,66 @@ public class Heaps {
         }
     }
 
-    public static void main(String[] args) {
-        Heap hp = new Heap();
-        hp.add(2);
-        hp.add(3);
-        hp.add(4);
-        hp.add(5);
-        hp.add(10);
-        hp.add(1);
+    public static void heapify(int arr[], int i, int size) {
+        int left = 2*i + 1;
+        int right = 2*i + 2;
+        int maxIdx = i;
 
-        while(!hp.isEmpty()) {
-            System.out.println(hp.peek());
-            hp.remove();
+        if(left < size && arr[left] > arr[maxIdx]) {
+            maxIdx = left;
+        }
+
+        if(right < size && arr[right] > arr[maxIdx]) {
+            maxIdx = right;
+        }
+
+        if(maxIdx != i) {
+            //swap
+            int temp = arr[i];
+            arr[i] = arr[maxIdx];
+            arr[maxIdx] = temp;
+
+            heapify(arr,maxIdx,size);
+        }
+    }
+
+    public static void heapSort(int arr[]) {
+        //step1 - build maxHeap
+        int n=arr.length;
+        for(int i=n/2; i>=0; i--) {
+            heapify(arr,i,n);
+        }
+
+        //step2 - push largest at end
+        for(int i=n-1; i>0; i--) {
+            //swap
+            int temp = arr[i];
+            arr[i] = arr[0];
+            arr[0] = temp;
+
+            heapify(arr,0,i);
+        }
+    }
+
+    public static void main(String[] args) {
+        // Heap hp = new Heap();
+        // hp.add(2);
+        // hp.add(3);
+        // hp.add(4);
+        // hp.add(5);
+        // hp.add(10);
+        // hp.add(1);
+
+        // while(!hp.isEmpty()) {
+        //     System.out.println(hp.peek());
+        //     hp.remove();
+        // }
+
+        int arr[] = {1,2,4,5,3};
+        heapSort(arr);
+
+        for(int i=0; i<arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
     }
 }
