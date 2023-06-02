d50ce5dd3021e050b4ab52878b616a586b0e1f3c
RuchiRanka <ruchiranka2103@gmail.com>
2023-06-02 22:59:11 +0530

    commit7

--- Java Programming Language/HashMapB.java
+++ Java Programming Language/HashMapB.java
@@ -1,6 +1,56 @@
 import java.util.*;
 
 public class HashMapB {
+    public static void majorityElem(int arr[]) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for(int i=0; i<arr.length; i++) {
+            // if(hm.containsKey(arr[i])) {
+            //     hm.put(arr[i], hm.get(arr[i])+1);
+            // }
+            // else {
+            //     hm.put(arr[i],1);
+            // }
+
+            hm.put(arr[i], hm.getOrDefault(arr[i], 0)+1);
+        }
+
+        // Set<Integer> keySet = hm.keySet();
+        for(Integer key : hm.keySet()) {
+            if(hm.get(key)>arr.length/3) {
+                System.out.println(key);
+            }
+        }
+    }
+
+    public static boolean validAnagrams(String s1, String s2) {
+        if(s1.length() != s2.length()) {
+            return false;
+        }
+
+        HashMap<Character, Integer> hm = new HashMap<>();
+        for(int i=0; i<s1.length(); i++) {
+            char ch = s1.charAt(i);
+            hm.put(ch, hm.getOrDefault(ch, 0)+1);
+        }
+
+        for(int i=0; i<s2.length(); i++) {
+            char ch = s2.charAt(i);
+            if(hm.get(ch) != null) {
+                if(hm.get(ch) == 1) {
+                    hm.remove(ch);
+                }
+                else {
+                    hm.put(ch, hm.get(ch)-1);
+                }
+            }
+            else {
+                return false;
+            }
+        }
+
+        return hm.isEmpty();
+    }
+
     public static void main(String[] args) {
         // //Create
         // HashMap<String, Integer> hm = new HashMap<>();
@@ -31,20 +81,43 @@ public class HashMapB {
         // hm.clear();
         // System.out.println(hm.isEmpty());
 
+        // HashMap<String, Integer> hm = new HashMap<>();
+        // hm.put("India",100);
+        // hm.put("Indonesia",10);
+        // hm.put("China",150);
+        // hm.put("USA",50);
+        // hm.put("Nepal",5);
+        // System.out.println(hm);
+        
+        // //Iterate
+        // Set<String> keys = hm.keySet();
+        // System.out.println(keys);
+        
+        // for(String k : keys) { //forEach Loop
+        //     System.out.println("key = " + k + ", value = " + hm.get(k));
+        // }
+            
+        // LinkedHashMap<String, Integer> lhm = new LinkedHashMap<>();
+        // lhm.put("India",100);
+        // lhm.put("Indonesia",10);
+        // lhm.put("China",150);
+        // lhm.put("USA",50);
+        // lhm.put("Nepal",5);
+        // System.out.println(lhm);
+        
+        // TreeMap<String, Integer> tm = new TreeMap<>();
+        // tm.put("India",100);
+        // tm.put("Indonesia",10);
+        // tm.put("China",150);
+        // tm.put("USA",50);
+        // tm.put("Nepal",5);
+        // System.out.println(tm);
 
-        HashMap<String, Integer> hm = new HashMap<>();
-        hm.put("India",100);
-        hm.put("Indonesia",10);
-        hm.put("China",150);
-        hm.put("USA",50);
-        hm.put("Nepal",5);
+        // int arr[] = {1,3,2,5,1,3,1,5,1};
+        // majorityElem(arr);
 
-        //Iterate
-        Set<String> keys = hm.keySet();
-        System.out.println(keys);
-
-        for(String k : keys) { //forEach Loop
-            System.out.println("key = " + k + ", value = " + hm.get(k));
-        }
+        String s1 = "race";
+        String s2 = "care";
+        System.out.println(validAnagrams(s1, s2));
     }
 }
---
+++ Java Programming Language/HashSetB.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class HashSetB {
+    public static void main(String[] args) {
+        // HashSet<Integer> set = new HashSet<>();
+        // set.add(1);
+        // set.add(2);
+        // set.add(4);
+        // set.add(2);
+        // set.add(1);
+
+        // System.out.println(set);
+
+        // if(set.contains(2)) {
+        //     System.out.println("Set contains 2");
+        // }
+        // if(!set.contains(3)) {
+        //     System.out.println("Set does not contain 3");
+        // }
+
+        // set.remove(2);
+        // System.out.println(set);
+        // System.out.println(set.size());
+        // System.out.println(set.isEmpty());
+        // set.clear();
+        // System.out.println(set.size());
+        // System.out.println(set.isEmpty());
+
+        // HashSet<String> cities = new HashSet<>();
+        // cities.add("Delhi");
+        // cities.add("Noida");
+        // cities.add("Pune");
+        // cities.add("Mumbai");
+
+        // Iterator it = cities.iterator();
+        // while(it.hasNext()) {
+        //     System.out.println(it.next());
+        // }
+
+        // for(String city: cities) {
+        //     System.out.println(city);
+        // }
+    }
+}
