7a6b70b6ba3e615d582e1ade02e820ab8e8e357e
RuchiRanka <ruchiranka2103@gmail.com>
2023-06-05 22:02:56 +0530

    commit11

--- Java Programming Language/HashSetB.java
+++ Java Programming Language/HashSetB.java
@@ -106,6 +106,21 @@ public class HashSetB {
         return len;
     }
 
+    public static int KSumSubarray(int arr[], int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0,1);
+        int sum = 0;
+        int ans = 0;
+        for(int j=0; j<arr.length; j++) {
+            sum += arr[j];
+            if(map.containsKey(sum-k)) {
+                ans += map.get(sum-k);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         // HashSet<Integer> set = new HashSet<>();
         // set.add(1);
@@ -180,6 +195,12 @@ public class HashSetB {
 
         // int arr[] = {15,-2,2,-8,1,7,10,23};
         // System.out.println(zeroSumSubarray(arr));
+
+        // int arr[] = {10,2,-2,-20,10};
+        // int k = -10;
+        int arr[] = {1,2,3};
+        int k = 3;
+        System.out.println(KSumSubarray(arr, k));
     }
 }
 
