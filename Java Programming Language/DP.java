dad703daaac05842001f5231c0a78c51d3c1dc5f
RuchiRanka <ruchiranka2103@gmail.com>
2023-06-26 00:02:07 +0530

    commit20

--- Java Programming Language/DP.java
+++ Java Programming Language/DP.java
@@ -283,7 +283,34 @@ public class DP {
             dp[n][m] = Math.max(ans1, ans2);
         }
         return dp[n][m];
-    } 
+    }
+
+    public static int lcsTab(String str1, String str2) {
+        int n = str1.length();
+        int m = str2.length();
+
+        int dp[][] = new int[n+1][m+1];
+
+        for(int i=1; i<dp.length; i++) {
+            for(int j=1; j<dp[0].length; j++) {
+                if(str1.charAt(i-1) == str2.charAt(j-1)) {
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                }
+                else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+
+        for(int i=0; i<dp.length; i++) {
+            for(int j=0; j<dp[0].length; j++) {
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
+        
+        return dp[n][m];
+    }
 
     public static void main(String[] args) {
         // int n=5;
@@ -347,6 +374,6 @@ public class DP {
                 dp[i][j] = -1;
             }
         }
-        System.out.println(lcsMemo(str1, str2, str1.length(), str2.length(), dp));
+        System.out.println(lcsTab(str1, str2));
     }
 }
